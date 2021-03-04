a2c6cf43ac3863e8aff3face1a991d10252b9a71
La Ode Safar <laodesafar94@gmail.com>
2021-03-04 08:36:37 +0700

    menambahkan section sambutan dan agenda

--- src/js/dropdown.js
+++ src/js/dropdown.js
@@ -1,13 +1,29 @@
 import { addClass, removeClass } from "./utils-class";
 
-const dropdownId = document.getElementById("dropdown");
-dropdownId.addEventListener("click", function () {
-  const dropdownMenuId = document.getElementById("dropdown-menu");
-  if (dropdownMenuId.className.indexOf("opacity-0") > -1) {
-    addClass(dropdownMenuId, "opacity-100 z-30 block");
-    removeClass(dropdownMenuId, "opacity-0 md:invisible hidden");
-  } else {
-    addClass(dropdownMenuId, "opacity-0 md:invisible hidden");
-    removeClass(dropdownMenuId, "opacity-100 z-30 block");
-  }
-});
+const dropdown = document.querySelector(".dropdown");
+
+  dropdown.addEventListener("click", function() {
+    const dropdownMenu = document.querySelector(".dropdown-menu");
+    if (dropdownMenu.className.indexOf("opacity-0") > -1) {
+      addClass(dropdownMenu, "opacity-100 z-30 block");
+      removeClass(dropdownMenu, "opacity-0 md:invisible hidden");
+    } else {
+      addClass(dropdownMenu, "opacity-0 md:invisible hidden");
+      removeClass(dropdownMenu, "opacity-100 z-30 block");
+    }
+  });
+
+  dropdown.addEventListener('mouseenter', (e) => {
+    const dropdownMenu = document.querySelector(".dropdown-menu");
+    if (dropdownMenu.className.indexOf("opacity-0") > -1) {
+      addClass(dropdownMenu, "opacity-100 z-30 block");
+      removeClass(dropdownMenu, "opacity-0 md:invisible hidden");
+    }
+  })
+  dropdown.addEventListener('mouseleave', (e) => {
+    const dropdownMenu = document.querySelector(".dropdown-menu");
+    if (!dropdownMenu.className.indexOf("opacity-0")> -1) {
+      addClass(dropdownMenu, "opacity-0 md:invisible hidden");
+      removeClass(dropdownMenu, "opacity-100 z-30 block");
+    }
+  })
